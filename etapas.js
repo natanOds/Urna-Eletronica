258c53091fb1ccb1e4221b38eff461cf4efc7eb2
natanOds <natanods4@gmail.com>
2021-03-21 21:55:53 -0300

    Terminado a etapa etapas feita em Json e começado a controlar a tela via script

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos:[
+      {
+        numero: 38111,
+        nome: 'Fulano de tal',
+        partido: 'ABC',
+        fotos:[
+          {url:'fotinhasacolocar.jpg', legenda: 'Vereador'}
+        ]
+      },
+      {
+        numero: 77222,
+        nome: "Beltra da Silva",
+        partido: 'DEFG',
+        fotos:[
+          {url:'fotinhastal.jpg', legenda: 'Vereador'}
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero:99,
+        nome: 'Ciclano',
+        partido: 'ABC',
+        vice: 'Cic',
+        fotos:[
+          {url: '99.jpg', legenda: 'Prefeito'},
+          {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: 84,
+        name: 'Zulano',
+        partido: 'QWERTY',
+        vice: 'Zul',
+        fotos: [
+          {url: 'fotos.jpg', legenda: 'Prefeito'},
+          {url: 'fotinhas.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+    ]
+  }
+];
